86b9b59c2f089f56dcee44363c169a0933035f88
Ellyn Butler <butellyn@galton.uphs.upenn.edu>
2019-05-02 11:00:18 -0400

    Woohoo

--- Longitudinal/firstpass.R
+++ Longitudinal/firstpass.R
@@ -3,8 +3,6 @@
 ### Ellyn Butler
 ### May 1, 2019
 
-args <- commandArgs(trailingOnly=TRUE)
-type <- args[1]
 
 # Load the data
 df <- read.csv("/home/adrose/forRuben/data/tmp/n2416_imagingDataDump_2018-09-20.csv")
@@ -145,217 +143,227 @@ df$bblid <- factor(df$bblid)
 ######################################################## Vars ########################################################
 
 
-if (type == "cort") {
-	xvars <- cortvars
-	fullname <- "Cortical Thickness"
-} else if (type == "gmv") {
-	xvars <- gmvvars
-	fullname <- "Gray Matter Volume"
-} else if (type == "gmd") {
-	xvars <- gmdvars
-	fullname <- "Gray Matter Density"
-} else if (type == "fa") {
-	xvars <- favars
-	fullname <- "Fractional Anisotropy"
-} else if (type == "md") {
-	xvars <- mdvars
-	fullname <- "Mean Diffusivity"
-} else if (type == "reho") {
-	xvars <- rehovars
-	fullname <- "Regional Homogeneity"
-} else if (type == "cbf") {
-	xvars <- cbfvars
-	fullname <- "Cerebral Blood Flow"
-} else if (type == "alff") {
-	xvars <- alffvars
-	fullname <- "Amplitude of Low Frequency Fluctuations"
-} 
+
 
 
 ############################ Females ############################
 
-df_F <- df[df$sex == 2, c("bblid", "psTerminal", "scanageMonths", xvars, yvar)]
-df_F$bblid <- as.factor(df_F$bblid)
+types <- c("cort", "gmv", "gmd", "fa", "md", "reho", "cbf", "alff")
+for (type in types) {
+	if (type == "cort") {
+		xvars <- cortvars
+		fullname <- "Cortical Thickness"
+	} else if (type == "gmv") {
+		xvars <- gmvvars
+		fullname <- "Gray Matter Volume"
+	} else if (type == "gmd") {
+		xvars <- gmdvars
+		fullname <- "Gray Matter Density"
+	} else if (type == "fa") {
+		xvars <- favars
+		fullname <- "Fractional Anisotropy"
+	} else if (type == "md") {
+		xvars <- mdvars
+		fullname <- "Mean Diffusivity"
+	} else if (type == "reho") {
+		xvars <- rehovars
+		fullname <- "Regional Homogeneity"
+	} else if (type == "cbf") {
+		xvars <- cbfvars
+		fullname <- "Cerebral Blood Flow"
+	} else if (type == "alff") {
+		xvars <- alffvars
+		fullname <- "Amplitude of Low Frequency Fluctuations"
+	} 
 
-for (i in 1:ncol(df_F)) {
-	na_index <- is.na(df_F[,i])
-	df_F <- df_F[!na_index,]
-}
-rownames(df_F) <- 1:nrow(df_F)
+	df_F <- df[df$sex == 2, c("bblid", "psTerminal", "scanageMonths", xvars, yvar)]
+	df_F$bblid <- as.factor(df_F$bblid)
 
-# Filter for people with more than one timepoint
-numrow_F <- nrow(df_F)
-keepvec <- rep(FALSE, numrow_F)
-for (i in 1:nrow(df_F)) {
-	person <- as.character(df_F[i, "bblid"])
-	numtimes <- nrow(df_F[df_F$bblid == person, ])
-	if (numtimes > 1) { keepvec[i] <- TRUE }
-}
-df_F <- df_F[keepvec,]
-rownames(df_F) <- 1:nrow(df_F)
-
-# Calculate brain slope parameters
-xvars_slopes <- paste0(xvars, "_slope")
-for (colname in xvars_slopes) { df_F[,colname] <- NA }
-df_F$bblid <- factor(df_F$bblid)
-for (i in 1:nrow(df_F)) {
-	person <- df_F[i, "bblid"]
-	tmp_df <- df_F[df_F$bblid == person, ]
-	age_vec <- tmp_df$scanageMonths
-	for (colname in xvars) {
-		slope_colname <- paste0(colname, "_slope")
-		brain_vec <- tmp_df[,colname]
-		df_F[i, slope_colname] <- lm(brain_vec ~ age_vec)$coefficients["age_vec"][[1]]
+	for (i in 1:ncol(df_F)) {
+		na_index <- is.na(df_F[,i])
+		df_F <- df_F[!na_index,]
 	}
-}
+	rownames(df_F) <- 1:nrow(df_F)
 
-# Filter the data so you only have the first timepoint raw brain data
-keep_vec <- rep(FALSE, nrow(df_F))
-for (i in 1:nrow(df_F)) {
-	person <- df_F[i, "bblid"]
-	minage <- min(df_F[df_F$bblid == person, "scanageMonths"])
-	if (df_F[i, "scanageMonths"] == minage) {
-		keep_vec[i] <- TRUE
+	# Filter for people with more than one timepoint
+	numrow_F <- nrow(df_F)
+	keepvec <- rep(FALSE, numrow_F)
+	for (i in 1:nrow(df_F)) {
+		person <- as.character(df_F[i, "bblid"])
+		numtimes <- nrow(df_F[df_F$bblid == person, ])
+		if (numtimes > 1) { keepvec[i] <- TRUE }
 	}
+	df_F <- df_F[keepvec,]
+	rownames(df_F) <- 1:nrow(df_F)
+
+	# Calculate brain slope parameters
+	xvars_slopes <- paste0(xvars, "_slope")
+	for (colname in xvars_slopes) { df_F[,colname] <- NA }
+	df_F$bblid <- factor(df_F$bblid)
+	for (i in 1:nrow(df_F)) {
+		person <- df_F[i, "bblid"]
+		tmp_df <- df_F[df_F$bblid == person, ]
+		age_vec <- tmp_df$scanageMonths
+		for (colname in xvars) {
+			slope_colname <- paste0(colname, "_slope")
+			brain_vec <- tmp_df[,colname]
+			df_F[i, slope_colname] <- lm(brain_vec ~ age_vec)$coefficients["age_vec"][[1]]
+		}
+	}
+
+	# Filter the data so you only have the first timepoint raw brain data
+	keep_vec <- rep(FALSE, nrow(df_F))
+	for (i in 1:nrow(df_F)) {
+		person <- df_F[i, "bblid"]
+		minage <- min(df_F[df_F$bblid == person, "scanageMonths"])
+		if (df_F[i, "scanageMonths"] == minage) {
+			keep_vec[i] <- TRUE
+		}
 	
-}
-df_F2 <- df_F[keep_vec,]
+	}
+	df_F2 <- df_F[keep_vec,]
 
-# Now build the models
-td_F <- df_F2[df_F2$psTerminal == "TD",]
-rownames(td_F) <- 1:nrow(td_F)
+	# Now build the models
+	td_F <- df_F2[df_F2$psTerminal == "TD",]
+	rownames(td_F) <- 1:nrow(td_F)
 
-folds5 <- createFolds(td_F$scanageMonths, k = 5, list = TRUE)
+	folds5 <- createFolds(td_F$scanageMonths, k = 5, list = TRUE)
 
-for (i in 1:5) {
-	test <- folds5[[i]]
-	trainfolds <- subset(folds5,!(grepl(i, names(folds5))))
-	train <- c()
-	for (j in 1:4) { train <- c(train, trainfolds[[j]]) }
-	assign(paste0("x_td_train_F_", i), td_F[train, c("bblid", xvars, xvars_slopes)])
-	assign(paste0("x_td_test_F_", i), td_F[test, c("bblid", xvars, xvars_slopes)])
-	assign(paste0("y_td_train_F_", i), td_F[train, c("bblid", yvar)])
-	assign(paste0("y_td_test_F_", i), td_F[test, c("bblid", yvar)])
-}
+	for (i in 1:5) {
+		test <- folds5[[i]]
+		trainfolds <- subset(folds5,!(grepl(i, names(folds5))))
+		train <- c()
+		for (j in 1:4) { train <- c(train, trainfolds[[j]]) }
+		assign(paste0("x_td_train_F_", i), td_F[train, c("bblid", xvars, xvars_slopes)])
+		assign(paste0("x_td_test_F_", i), td_F[test, c("bblid", xvars, xvars_slopes)])
+		assign(paste0("y_td_train_F_", i), td_F[train, c("bblid", yvar)])
+		assign(paste0("y_td_test_F_", i), td_F[test, c("bblid", yvar)])
+	}
 
-#####Elastic Net: out of sample predictions for TD's
-for (i in 1:5) {
-	x_train_df <- get(paste0("x_td_train_F_", i))
-	x_train_input <- x_train_df[,c(xvars, xvars_slopes)]
-	y_train_df <- get(paste0("y_td_train_F_", i))
-	y_train_input <- y_train_df$ageAtTerminalScan
-	ageAtTerminalScan <- y_train_input
+	#####Elastic Net: out of sample predictions for TD's
+	for (i in 1:5) {
+		x_train_df <- get(paste0("x_td_train_F_", i))
+		x_train_input <- x_train_df[,c(xvars, xvars_slopes)]
+		y_train_df <- get(paste0("y_td_train_F_", i))
+		y_train_input <- y_train_df$ageAtTerminalScan
+		ageAtTerminalScan <- y_train_input
+		train_control <- trainControl(method = "repeatedcv", number = 5, repeats = 5, search = "random", verboseIter = FALSE)
+		elastic_net_model <- train(ageAtTerminalScan ~ . , data = cbind(ageAtTerminalScan, x_train_input), method = "glmnet", 
+					preProcess = c("center", "scale"), tuneLength = 25, trControl = train_control) 
+					# just used parameters from here: https://www.datacamp.com/community/tutorials/tutorial-ridge-lasso-elastic-net
+		assign(paste0("mod_td_F_", i), elastic_net_model)
+		x_test_df <- get(paste0("x_td_test_F_", i))
+		y_test_df <- get(paste0("y_td_test_F_", i))
+		y_test_predicted <- predict(elastic_net_model, x_test_df)
+		if (i == 1) { 
+			predicted_values <- merge(cbind(x_test_df, y_test_predicted), y_test_df)
+		} else { 
+			predicted_values <- rbind(predicted_values, merge(cbind(x_test_df, y_test_predicted), y_test_df))
+		}
+	}
+	names(predicted_values)[names(predicted_values) == 'y_test_predicted'] <- 'predicted_ageAtTerminalScan'
+	
+	#####Elastic Net: predictions for PS's 
+	# train on full TD set
+	x_td_train_input <- td_F[,c(xvars, xvars_slopes)]
+	ageAtTerminalScan <- td_F[,yvar]
 	train_control <- trainControl(method = "repeatedcv", number = 5, repeats = 5, search = "random", verboseIter = FALSE)
-	elastic_net_model <- train(ageAtTerminalScan ~ . , data = cbind(ageAtTerminalScan, x_train_input), method = "glmnet", 
-				preProcess = c("center", "scale"), tuneLength = 25, trControl = train_control) 
-				# just used parameters from here: https://www.datacamp.com/community/tutorials/tutorial-ridge-lasso-elastic-net
-	assign(paste0("mod_td_F_", i), elastic_net_model)
-	x_test_df <- get(paste0("x_td_test_F_", i))
-	y_test_df <- get(paste0("y_td_test_F_", i))
-	y_test_predicted <- predict(elastic_net_model, x_test_df)
-	if (i == 1) { 
-		predicted_values <- merge(cbind(x_test_df, y_test_predicted), y_test_df)
-	} else { 
-		predicted_values <- rbind(predicted_values, merge(cbind(x_test_df, y_test_predicted), y_test_df))
+	elastic_net_model <- train(ageAtTerminalScan ~ . , data = cbind(ageAtTerminalScan, x_td_train_input), method = "glmnet", 
+					preProcess = c("center", "scale"), tuneLength = 25, trControl = train_control) 
+
+	# predict on PS's
+	x_ps_F <- df_F2[df_F2$psTerminal == "PS", c("bblid", xvars, xvars_slopes)]
+	y_ps_F <- df_F2[df_F2$psTerminal == "PS", c("bblid", yvar)]
+	x_ps_input <- x_ps_F[,c(xvars, xvars_slopes)]
+	y_ps_input <- y_ps_F[,yvar]
+	ageAtTerminalScan <- y_ps_input
+	y_ps_predicted <- predict(elastic_net_model, x_ps_input)
+	x_ps_F$predicted_ageAtTerminalScan <- y_ps_predicted ### March 12, 2019: THIS HAS TO BE HERE
+
+	# create final dfs
+	df_td_F <- predicted_values
+	df_ps_F <- merge(x_ps_F, y_ps_F) #####
+
+	# Create years age variables
+	df_ps_F$real_age <- df_ps_F$ageAtTerminalScan/12
+	df_ps_F$pred_age <- df_ps_F$predicted_ageAtTerminalScan/12
+	df_td_F$real_age <- df_td_F$ageAtTerminalScan/12
+	df_td_F$pred_age <- df_td_F$predicted_ageAtTerminalScan/12
+
+	# Create the final dataframe
+	df_ps_F$diagnosis <- "PS"
+	df_td_F$diagnosis <- "TD"
+	df_both_F <- rbind(df_ps_F, df_td_F)
+	df_both_F$diagnosis <- as.factor(df_both_F$diagnosis)
+
+	# Regress real age out of predicted age
+	df_both_F$pred_age_regressed <- lm(pred_age ~ real_age, data=df_both_F)$residuals
+
+
+
+	######### Two-Year Age Bins: Mean Difference between Real and Predicted Ages 
+	df_both_F$age_cat <- 0
+	df_both_F$pred_adult <- 0
+
+	rownames(df_both_F) <- 1:nrow(df_both_F) # change row indices
+
+	for (row in 1:nrow(df_both_F)) {
+		if (df_both_F[row,"real_age"] < 10) { df_both_F[row,"age_cat"] = "8_9" }
+		else if (df_both_F[row,"real_age"] >= 10 & df_both_F[row,"real_age"] < 12) { df_both_F[row,"age_cat"] = "10_11" }
+		else if (df_both_F[row,"real_age"] >= 12 & df_both_F[row,"real_age"] < 14) { df_both_F[row,"age_cat"] = "12_13" }
+		else if (df_both_F[row,"real_age"] >= 14 & df_both_F[row,"real_age"] < 16) { df_both_F[row,"age_cat"] = "14_15" }
+		else if (df_both_F[row,"real_age"] >= 16 & df_both_F[row,"real_age"] < 18) { df_both_F[row,"age_cat"] = "16_17" }
+		else if (df_both_F[row,"real_age"] >= 18 & df_both_F[row,"real_age"] < 20) { df_both_F[row,"age_cat"] = "18_19" }
+		else if (df_both_F[row,"real_age"] >= 20 & df_both_F[row,"real_age"] < 24) { df_both_F[row,"age_cat"] = "20_23" }
+		if (df_both_F[row,"pred_age"] >= 18) { df_both_F[row,"pred_adult"] = 1 }
 	}
-}
-names(predicted_values)[names(predicted_values) == 'y_test_predicted'] <- 'predicted_ageAtTerminalScan'
-	
-#####Elastic Net: predictions for PS's 
-# train on full TD set
-x_td_train_input <- td_F[,c(xvars, xvars_slopes)]
-ageAtTerminalScan <- td_F[,yvar]
-train_control <- trainControl(method = "repeatedcv", number = 5, repeats = 5, search = "random", verboseIter = FALSE)
-elastic_net_model <- train(ageAtTerminalScan ~ . , data = cbind(ageAtTerminalScan, x_td_train_input), method = "glmnet", 
-				preProcess = c("center", "scale"), tuneLength = 25, trControl = train_control) 
 
-# predict on PS's
-x_ps_F <- df_F[df_F$psTerminal == "PS", c("bblid", xvars, xvars_slopes)]
-y_ps_F <- df_F[df_F$psTerminal == "PS", c("bblid", yvar)]
-x_ps_input <- x_ps_F[,c(xvars, xvars_slopes)]
-y_ps_input <- y_ps_F[,yvar]
-ageAtTerminalScan <- y_ps_input
-y_ps_predicted <- predict(elastic_net_model, x_ps_input)
-x_ps_F$predicted_ageAtTerminalScan <- y_ps_predicted ### March 12, 2019: THIS HAS TO BE HERE
-
-# create final dfs
-df_td_F <- predicted_values
-df_ps_F <- merge(x_ps_F, y_ps_F) #####
-
-# Create years age variables
-df_ps_F$real_age <- df_ps_F$ageAtTerminalScan/12
-df_ps_F$pred_age <- df_ps_F$predicted_ageAtTerminalScan/12
-df_td_F$real_age <- df_td_F$ageAtTerminalScan/12
-df_td_F$pred_age <- df_td_F$predicted_ageAtTerminalScan/12
-
-# Create the final dataframe
-df_ps_F$diagnosis <- "PS"
-df_td_F$diagnosis <- "TD"
-df_both_F <- rbind(df_ps_F, df_td_F)
-df_both_F$diagnosis <- as.factor(df_both_F$diagnosis)
+	df_both_F$age_cat <- as.factor(df_both_F$age_cat)
 
 
 
-######### Two-Year Age Bins: Mean Difference between Real and Predicted Ages 
-df_both_F$age_cat <- 0
-df_both_F$pred_adult <- 0
+	summary_2YBins_predreg_F <- data.frame(matrix(0, nrow=10, ncol=5))
+	colnames(summary_2YBins_predreg_F) <- c("Age_Category", "Diagnosis", "Pred_Age_Regressed", "N", "SE")
+	summary_2YBins_predreg_F$Age_Category <- c("12_13", "12_13", "14_15", "14_15", "16_17", "16_17", "18_19", "18_19", "20_23", "20_23")
+	summary_2YBins_predreg_F$Diagnosis <- c("TD", "PS", "TD", "PS", "TD", "PS", "TD", "PS", "TD", "PS")
 
-rownames(df_both_F) <- 1:nrow(df_both_F) # change row indices
+	for (i in 1:nrow(summary_2YBins_predreg_F)) { #CIs from conf.level
+		agebin <- summary_2YBins_predreg_F[i, "Age_Category"]
+		diag <- summary_2YBins_predreg_F[i, "Diagnosis"]
+		# Total number 
+		N <- length(df_both_F[df_both_F$age_cat == agebin & df_both_F$diagnosis == diag, "age_cat"])
+		### For plot
+		# Mean Difference between Real and Predicted Ages
+		predagereg <- df_both_F[df_both_F$age_cat == agebin & df_both_F$diagnosis == diag, "pred_age_regressed"]
+		Predagereg <- mean(predagereg)
+		# Put values in dataframe
+		summary_2YBins_predreg_F[i, "N"] <- N
+		summary_2YBins_predreg_F[i, "Pred_Age_Regressed"] <- Predagereg
+		# SE
+		summary_2YBins_predreg_F[i, "SE"] <- sqrt(sd(predagereg)/N)
+	}
+	summary_2YBins_predreg_F$Age_Category <- factor(summary_2YBins_predreg_F$Age_Category, levels = c("12_13", "14_15", "16_17", "18_19", "20_23"))
 
-for (row in 1:nrow(df_both_F)) {
-	if (df_both_F[row,"real_age"] < 10) { df_both_F[row,"age_cat"] = "8_9" }
-	else if (df_both_F[row,"real_age"] >= 10 & df_both_F[row,"real_age"] < 12) { df_both_F[row,"age_cat"] = "10_11" }
-	else if (df_both_F[row,"real_age"] >= 12 & df_both_F[row,"real_age"] < 14) { df_both_F[row,"age_cat"] = "12_13" }
-	else if (df_both_F[row,"real_age"] >= 14 & df_both_F[row,"real_age"] < 16) { df_both_F[row,"age_cat"] = "14_15" }
-	else if (df_both_F[row,"real_age"] >= 16 & df_both_F[row,"real_age"] < 18) { df_both_F[row,"age_cat"] = "16_17" }
-	else if (df_both_F[row,"real_age"] >= 18 & df_both_F[row,"real_age"] < 20) { df_both_F[row,"age_cat"] = "18_19" }
-	else if (df_both_F[row,"real_age"] >= 20 & df_both_F[row,"real_age"] < 24) { df_both_F[row,"age_cat"] = "20_23" }
-	if (df_both_F[row,"pred_age"] >= 18) { df_both_F[row,"pred_adult"] = 1 }
+	minrow_2YBins_predreg_F <- round(min(summary_2YBins_predreg_F$Pred_Age_Regressed - 2*summary_2YBins_predreg_F$SE, na.rm=TRUE)-1)
+	maxrow_2YBins_predreg_F <- round(max(summary_2YBins_predreg_F$Pred_Age_Regressed + 2*summary_2YBins_predreg_F$SE, na.rm=TRUE)+1)
+	bar_2YBins_predreg_F <- ggplot(data=summary_2YBins_predreg_F, aes(x=Age_Category, y=Pred_Age_Regressed, fill=Diagnosis)) +
+		geom_bar(stat="identity", position=position_dodge()) +
+		geom_errorbar(mapping=aes(ymin=Pred_Age_Regressed+2*SE, ymax=Pred_Age_Regressed-2*SE), position=position_dodge()) +
+		geom_text(aes(label=N), vjust=1.6, color="black", position = position_dodge(0.9), size=4) +
+		scale_fill_brewer(palette="Paired") + 
+		ggtitle(fullname) + xlab("Age Category") + ylab("Predicted Terminal Age (Regressed; 95% CI)") +
+		scale_y_continuous(limits = c(minrow_2YBins_predreg_F, maxrow_2YBins_predreg_F), breaks = seq(minrow_2YBins_predreg_F, maxrow_2YBins_predreg_F, by=1)) +
+		theme(plot.title = element_text(family="Times", face="bold", size=18))
+	assign(paste0(type, "_p"), bar_2YBins_predreg_)
 }
 
-df_both_F$age_cat <- as.factor(df_both_F$age_cat)
 
-
-
-summary_2YBins_diffRealPred_F <- data.frame(matrix(0, nrow=14, ncol=5))
-colnames(summary_2YBins_diffRealPred_F) <- c("Age_Category", "Diagnosis", "Mean_Diff_Real_minus_Pred", "N", "SE")
-summary_2YBins_diffRealPred_F$Age_Category <- c("8_9", "8_9", "10_11", "10_11", "12_13", "12_13", "14_15", "14_15", "16_17", "16_17", "18_19", "18_19", "20_23", "20_23")
-summary_2YBins_diffRealPred_F$Diagnosis <- c("TD", "PS", "TD", "PS", "TD", "PS", "TD", "PS", "TD", "PS", "TD", "PS", "TD", "PS")
-
-for (i in 1:nrow(summary_2YBins_diffRealPred_F)) { #CIs from conf.level
-	agebin <- summary_2YBins_diffRealPred_F[i, "Age_Category"]
-	diag <- summary_2YBins_diffRealPred_F[i, "Diagnosis"]
-	# Total number 
-	N <- length(df_both_F[df_both_F$age_cat == agebin & df_both_F$diagnosis == diag, "age_cat"])
-	### For plot
-	# Mean Difference between Real and Predicted Ages
-	realpreddiff <- df_both_F[df_both_F$age_cat == agebin & df_both_F$diagnosis == diag, "real_age"] - df_both_F[df_both_F$age_cat == agebin & df_both_F$diagnosis == diag, "pred_age"]
-	RealPredDiff <- mean(realpreddiff)
-	# Put values in dataframe
-	summary_2YBins_diffRealPred_F[i, "N"] <- N
-	summary_2YBins_diffRealPred_F[i, "Mean_Diff_Real_minus_Pred"] <- RealPredDiff
-	# SE
-	summary_2YBins_diffRealPred_F[i, "SE"] <- sqrt(sd(realpreddiff)/N)
-}
-summary_2YBins_diffRealPred_F$Age_Category <- factor(summary_2YBins_diffRealPred_F$Age_Category, levels = c("8_9", "10_11", "12_13", "14_15", "16_17", "18_19", "20_23"))
-
-
-
-
-
-minrow_2YBins_diffRealPred_F <- round(min(summary_2YBins_diffRealPred_F$Mean_Diff_Real_minus_Pred - 2*summary_2YBins_diffRealPred_F$SE, na.rm=TRUE)-1)
-maxrow_2YBins_diffRealPred_F <- round(max(summary_2YBins_diffRealPred_F$Mean_Diff_Real_minus_Pred + 2*summary_2YBins_diffRealPred_F$SE, na.rm=TRUE)+1)
-bar_2YBins_diffRealPred_F <- ggplot(data=summary_2YBins_diffRealPred_F, aes(x=Age_Category, y=Mean_Diff_Real_minus_Pred, fill=Diagnosis)) +
-	geom_bar(stat="identity", position=position_dodge()) +
-	geom_errorbar(mapping=aes(ymin=Mean_Diff_Real_minus_Pred+2*SE, ymax=Mean_Diff_Real_minus_Pred-2*SE), position=position_dodge()) +
-	geom_text(aes(label=N), vjust=1.6, color="black", position = position_dodge(0.9), size=4) +
-	scale_fill_brewer(palette="Paired") + 
-	ggtitle("Real-Predicted Terminal Age") + xlab("Age Category") + ylab("Real - Pred Age (95% CI)") +
-	scale_y_continuous(limits = c(minrow_2YBins_diffRealPred_F, maxrow_2YBins_diffRealPred_F), breaks = seq(minrow_2YBins_diffRealPred_F, maxrow_2YBins_diffRealPred_F, by=1)) +
-	theme(plot.title = element_text(family="Times", face="bold", size=18))
-
-
-
+pdf(file="/home/butellyn/age_prediction/plots/longitudinal_usingslopes_F.pdf", width=16, height=6)
+grid.arrange(alff_p, cbf_p, ncol=2)
+grid.arrange(cort_p, fa_p, ncol=2)
+grid.arrange(gmd_p, gmv_p, ncol=2)
+grid.arrange(md_p, reho_p, ncol=2)
+dev.off()
 
 
 
